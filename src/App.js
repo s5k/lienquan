6bed93aaa505bd612fc8ee3cf16e3e2722e71eba
Quý Phạm <yungbloodx02@gmail.com>
2019-10-06 22:39:42 +0700

    refactor sidebar

--- src/App.js
+++ src/App.js
@@ -1,119 +1,30 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import {
-  faHome,
-  faTrophy,
-  faList,
-  faFile,
-  faNewspaper,
-  faBell,
-  faUsers,
-  faImage,
-  faTimes,
-  faBars,
-  faArrowUp,
-  faArrowDown
-} from '@fortawesome/free-solid-svg-icons'
-import logoAic from './images/logo-aic.png'
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
 import Homepage from './containers/Homepage/Homepage'
 import About from './containers/About/About'
 import Prize from './containers/Prize/Prize'
 import Schedule from './containers/Schedule/Schedule'
+import Sidebar from './containers/Sidebar'
 
 export default function() {
-  const [isSidebarOpen, setCollapseSidebar] = useState(false)
-
-  library.add(
-    fab,
-    faHome,
-    faTrophy,
-    faList,
-    faFile,
-    faNewspaper,
-    faBell,
-    faUsers,
-    faImage,
-    faTimes,
-    faBars,
-    faArrowUp,
-    faArrowDown
-  )
+  library.add(faArrowUp, faArrowDown)
 
   return (
     <Router>
       <div className="scroll-up-menu">
-        <FontAwesomeIcon icon="arrow-up"/>
+        <FontAwesomeIcon icon="arrow-up" />
       </div>
-      <div className={isSidebarOpen ? 'sidebar open-sidebar' : 'sidebar'}>
-        <div className="top-sidebar">
-          <div className="btn-close-menu">
-            <img src={logoAic} alt="Logo AIC" className="logo-aic" />
-            <FontAwesomeIcon
-              icon="times"
-              className="close-menu"
-              onClick={() => setCollapseSidebar(false)}
-            />
-          </div>
-          <div className="bnt-sidebar">
-            <FontAwesomeIcon
-              icon="bars"
-              className="bnt-sidebar"
-              onClick={() => setCollapseSidebar(true)}
-            />
-          </div>
-        </div>
-        <ul>
-          <li className="active">
-            <Link to="/">
-              <FontAwesomeIcon icon="home" /> <span>Home</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/prize">
-              <FontAwesomeIcon icon="trophy" /> <span>Prize Pool</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/schedule">
-              <FontAwesomeIcon icon="list" /> <span>Schedule</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              <FontAwesomeIcon icon="newspaper" /> <span>News</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              <FontAwesomeIcon icon="bell" /> <span>Evnets</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              <FontAwesomeIcon icon="users" />
-              <span>Teams</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              {' '}
-              <FontAwesomeIcon icon="image" />
-              <span>Photos & videos</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <Sidebar />
       <div className="language">
         <select className="select-language">
-          <option defaultValue="grapefruit">North America (Englsh)</option>
-          <option defaultValue="vietnames">Vietnames</option>
-          <option defaultValue="coconut">Coconut</option>
-          <option defaultValue="mango">Mango</option>
+          <option defaultValue="english">North America (English)</option>
+          <option defaultValue="vietnames">Vietnamese (Tiếng Việt)</option>
         </select>
       </div>
       {/* Thêm một trang mới, thì anh làm giống trang About nhé ('exact' để chỉ định trang chủ) */}
---
+++ src/containers/Sidebar/index.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faTrophy,
+  faList,
+  faFile,
+  faNewspaper,
+  faBell,
+  faUsers,
+  faImage,
+  faTimes,
+  faBars,
+  faArrowUp,
+  faArrowDown
+} from '@fortawesome/free-solid-svg-icons'
+
+import logoAic from '../../images/logo-aic.png'
+
+export default () => {
+  const [isSidebarOpen, setCollapseSidebar] = useState(false)
+
+  const [listMenu] = useState([
+    {
+      id: 0,
+      name: 'Home',
+      icon: 'home'
+    },
+    {
+      id: 1,
+      name: 'Prize Pool',
+      icon: 'trophy'
+    },
+    {
+      id: 2,
+      name: 'Schedule',
+      icon: 'list'
+    },
+    {
+      id: 3,
+      name: 'News',
+      icon: 'newspaper'
+    },
+    {
+      id: 4,
+      name: 'Events',
+      icon: 'bell'
+    },
+    {
+      id: 5,
+      name: 'Teams',
+      icon: 'users'
+    },
+    {
+      id: 6,
+      name: 'Photos & videos',
+      icon: 'image'
+    }
+  ])
+
+  library.add(
+    fab,
+    faHome,
+    faTrophy,
+    faList,
+    faFile,
+    faNewspaper,
+    faBell,
+    faUsers,
+    faImage,
+    faTimes,
+    faBars,
+    faArrowUp,
+    faArrowDown
+  )
+
+  return (
+    <div className={isSidebarOpen ? 'sidebar open-sidebar' : 'sidebar'}>
+      <div className="top-sidebar">
+        <div className="btn-close-menu">
+          <img src={logoAic} alt="Logo AIC" className="logo-aic" />
+          <FontAwesomeIcon
+            icon="times"
+            className="close-menu"
+            onClick={() => setCollapseSidebar(false)}
+          />
+        </div>
+        <div className="bnt-sidebar">
+          <FontAwesomeIcon
+            icon="bars"
+            className="bnt-sidebar"
+            onClick={() => setCollapseSidebar(true)}
+          />
+        </div>
+      </div>
+      <ul>
+        {listMenu.map((item, key) => {
+          return (
+            <li className={key === 0 ? 'active' : ''} key={key}>
+              <Link to="/">
+                <FontAwesomeIcon icon={item.icon} /> <span>{item.name}</span>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
