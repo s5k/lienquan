f7b0aa1fea5c3efe6ae6e5124984a2e32b3ef060
thetran94 <the12794@gmail.com>
2019-10-13 12:12:10 +0700

    update photo v2

--- src/containers/Photos/Photodetail.js
+++ src/containers/Photos/Photodetail.js
@@ -5,29 +5,59 @@
  */
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React,{ useState } from 'react'
 // import { Link } from 'react-router-dom'
+import FsLightbox from 'fslightbox-react';
 import './photos.css'
 
 export default () => {
+  const [toggler, setToggler] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
+  const toggleImage = (index) => {
+    setToggler(!toggler)
+    setImageIndex(index)
+  }
+  const hinhanh = [
+    [
+      'https://images.unsplash.com/photo-1519455953755-af066f52f1a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      'https://www.youtube.com/watch?v=xshEZzpS4CQ',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    ],
+    [
+      'https://i.imgur.com/fsyrScY.jpg',
+      'https://www.youtube.com/watch?v=xshEZzpS4CQ',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    ],
+    [
+      'https://i.imgur.com/fsyrScY.jpg',
+      'https://www.youtube.com/watch?v=xshEZzpS4CQ',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    ]
+  ]
   return (
     <div className="photodetailpage">
+      <FsLightbox
+        toggler={ toggler }
+        sources={ hinhanh[imageIndex] }
+        key={ imageIndex }
+      />
       <div className="photo-details">
-        <div className="photo-details-item">
+        <div className="photo-details-item" onClick={ () => toggleImage(0)}>
+          <img src={require('../../images/photodetail.jpg')} alt="facebook" />
+          <div className="photo-text">
+            <p className="photo-details-item-title">aic 2019 group stage day 1 </p>
+            <p className="photo-details-item-total">95 photos</p>
+          </div>
+          
+        </div>
+        <div className="photo-details-item" onClick={ () => toggleImage(1)}>
           <img src={require('../../images/photodetail.jpg')} alt="facebook" />
           <div className="photo-text">
             <p className="photo-details-item-title">aic 2019 group stage day 1 </p>
             <p className="photo-details-item-total">95 photos</p>
           </div>
         </div>
-        <div className="photo-details-item">
-          <img src={require('../../images/photodetail.jpg')} alt="facebook" />
-          <div className="photo-text">
-            <p className="photo-details-item-title">aic 2019 group stage day 1 </p>
-            <p className="photo-details-item-total">95 photos</p>
-          </div>
-        </div>
-        <div className="photo-details-item">
+        <div className="photo-details-item" onClick={ () => toggleImage(2)}>
           <img src={require('../../images/photodetail.jpg')} alt="facebook" />
           <div className="photo-text">
             <p className="photo-details-item-title">aic 2019 group stage day 1 </p>
--- src/containers/Photos/Photovideo.js
+++ src/containers/Photos/Photovideo.js
@@ -5,25 +5,57 @@
  */
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React,{ useState } from 'react'
 // import { Link } from 'react-router-dom'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import FsLightbox from 'fslightbox-react';
 import './photos.css'
 
 export default () => {
+  const [toggler, setToggler] = useState(false);
+  const [imageVideo, setImageVideo] = useState(0);
+
+  const hinhanh = [
+    "https://www.youtube.com/watch?v=qYjB584rhtM",
+    "https://www.youtube.com/watch?v=qILhS-Vvd2U",
+    "https://www.youtube.com/watch?v=lfHvC8yd9Sw",
+  ]
+
+  const toggleVideo = (index) => {
+    setToggler(!toggler)
+    setImageVideo(index)
+  }
+
   return (
     <div className="photovideopage">
       <div className="photo-video-items">
         <div className="video-heighlight">
+          <FsLightbox
+            toggler={ toggler }
+            sources={ [hinhanh[imageVideo]] }
+            key={ imageVideo }
+          />
           <Slider 
             arrows={false}
             dots={true}
+            swipe={false}
           >
             <div>
               <div className="video-heighlight-item">
-                <div className="video-heightlight-clip">
+                <div className="video-heightlight-clip" onClick={ () => toggleVideo(0) }>
+                  <img src={require('../../images/photovideo.jpg')} alt="photovideo"  />
+                </div>
+                <div className="video-heightlight-text">
+                  <p className="video-text-title">aic 2019 format annoucement and venue</p>
+                  <p className="video-text-des">Find out the details of AIC 2019 Events</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="video-heighlight-item">
+                <div className="video-heightlight-clip" onClick={ () => toggleVideo(1) }>
                   <img src={require('../../images/photovideo.jpg')} alt="photovideo" />
                 </div>
                 <div className="video-heightlight-text">
@@ -34,18 +66,7 @@ export default () => {
             </div>
             <div>
               <div className="video-heighlight-item">
-                <div className="video-heightlight-clip">
-                  <img src={require('../../images/photovideo.jpg')} alt="photovideo" />
-                </div>
-                <div className="video-heightlight-text">
-                  <p className="video-text-title">aic 2019 format annoucement and venue</p>
-                  <p className="video-text-des">Find out the details of AIC 2019 Events</p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="video-heighlight-item">
-                <div className="video-heightlight-clip">
+                <div className="video-heightlight-clip" onClick={ () => toggleVideo(2) }>
                   <img src={require('../../images/photovideo.jpg')} alt="photovideo" />
                 </div>
                 <div className="video-heightlight-text">
