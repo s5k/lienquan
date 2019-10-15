913718bb59e8e9b9bb60eb58db024103cd665457
Quý Phạm <yungbloodx02@gmail.com>
2019-10-15 15:37:20 +0700

    bỏ 1 slide team

--- src/containers/Teams/Teams.js
+++ src/containers/Teams/Teams.js
@@ -27,9 +27,7 @@ import { useStateValue } from '../../State'
 export default () => {
   const [{ teams }] = useStateValue()
 
-  const [nav1, setNav1] = useState(null)
-  const [nav2, setNav2] = useState(null)
-
+  const [teamIndex, setTeamIndex] = useState(0)
   library.add(fab, faArrowUp, faArrowDown, faCircle, faPlayCircle)
 
   return (
@@ -42,70 +40,54 @@ export default () => {
       </div>
       <h3 className="title-teams">MEET THE TEAMS</h3>
       <div className="teamspage-items">
-        <Slider
-          asNavFor={nav2}
-          ref={slider => setNav1(slider)}
-          arrows={false}
-          focusOnSelect={false}
-          infinite={true}
-          swipe={false}
-          initialSlide={0}
-          className="teams-info"
-        >
-          {teams.map((item, key) => (
-            <div key={key}>
-              <div className="teams-info-item">
-                <div className="team-member">
-                  <Slider centerMode={true} centerPadding={'30%'}>
-                    {item.players
-                      .sort((a, b) =>
-                        b.position !== '' &&
-                        a.position !== '' &&
-                        a.position > b.position
-                          ? 1
-                          : -1
-                      )
-                      .map((member, keyChild) => (
-                        <div key={keyChild}>
-                          <div className="member-info">
-                            <img className="member-img" src={member.image} />
-                          </div>
-                          <div className="member-info-text">
-                            <img
-                              className="member-lane"
-                              src={`../../images/icons/lane-${member.description
-                                .split(' ')
-                                .join('-')
-                                .toLowerCase()}.png`}
-                            />
-                            <p className="member-name">{member.name}</p>
-                            <p className="lane-name">{member.description}</p>
-                          </div>
-                        </div>
-                      ))}
-                  </Slider>
-                </div>
-                <div className="team-info">
-                  <img className="logo-team" src={item.logo} />
-                  <div className="team-info-content">
-                    <div className="title-team-name">{item.name}</div>
-                    <div className="team-area">{item.region}</div>
-                    <div className="team-des">{item.description}</div>
-                    <div className="team-video">
-                      <a href={item.video_link} target="blank">
-                        {' '}
-                        <FontAwesomeIcon icon="play-circle" /> WATCH VIDEO{' '}
-                      </a>
+        <div className="teams-info">
+          <div className="teams-info-item">
+            <div className="team-member">
+              <Slider centerMode={true} centerPadding={'30%'} initialSlide={0}>
+                {teams[teamIndex].players
+                  .sort((a, b) => {
+                    return a.position !== '' &&
+                      parseInt(a.position) > parseInt(b.position)
+                      ? true
+                      : false
+                  })
+                  .map((member, keyChild) => (
+                    <div key={keyChild}>
+                      <div className="member-info">
+                        <img className="member-img" src={member.image} />
+                      </div>
+                      <div className="member-info-text">
+                        <img
+                          className="member-lane"
+                          src={`../../images/icons/lane-${member.description
+                            .split(' ')
+                            .join('-')
+                            .toLowerCase()}.png`}
+                        />
+                        <p className="member-name">{member.name}</p>
+                        <p className="lane-name">{member.description}</p>
+                      </div>
                     </div>
-                  </div>
+                  ))}
+              </Slider>
+            </div>
+            <div className="team-info">
+              <img className="logo-team" src={teams[teamIndex].logo} />
+              <div className="team-info-content">
+                <div className="title-team-name">{teams[teamIndex].name}</div>
+                <div className="team-area">{teams[teamIndex].region}</div>
+                <div className="team-des">{teams[teamIndex].description}</div>
+                <div className="team-video">
+                  <a href={teams[teamIndex].video_link} target="blank">
+                    {' '}
+                    <FontAwesomeIcon icon="play-circle" /> WATCH VIDEO{' '}
+                  </a>
                 </div>
               </div>
             </div>
-          ))}
-        </Slider>
+          </div>
+        </div>
         <Slider
-          asNavFor={nav1}
-          ref={slider => setNav2(slider)}
           slidesToShow={teams.length > 12 ? 12 : teams.length - 1}
           focusOnSelect={true}
           centerMode={false}
@@ -131,10 +113,13 @@ export default () => {
               }
             }
           ]}
+          afterChange={index => setTeamIndex(index)}
         >
           {teams.map((item, key) => (
             <div key={key}>
-              <div className="team-name">{item.name}</div>
+              <div className="team-name" onClick={() => setTeamIndex(key)}>
+                {item.name}
+              </div>
             </div>
           ))}
         </Slider>
