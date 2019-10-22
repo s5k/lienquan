e1d9f3344ab0f3c4f815f50a1cc435ab8f057e5c
thetran94 <the12794@gmail.com>
2019-10-22 16:08:45 +0700

    update text home, GA code and sahre facebook

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -45,15 +45,7 @@ export default () => {
           className="cup-aic"
         />
         <p className="intro-content">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          Arena of Valor International Championship (AIC) 2019, giải đấu thể thao điện tử quốc tế bộ môn Liên Quân Mobile được tổ chức bởi Garena và Tencent sẽ diễn ra tại Thái Lan từ 05/11 tới 24/11 với tổng giải thưởng gần 12 tỷ đồng. 12 đội tuyển tới từ 9 khu vực hàng đầu trên thế giới sẽ cùng nhau tranh tài tại một trong những giải eSports quốc tế có số tiền thưởng lớn nhất thế giới ở thể loại MOBA trên nền tảng di động. Đặc biệt lần đầu tiên trong lịch sử AIC, giải đấu 1v1 sẽ được tổ chức để các siêu sao hàng đầu thế giới trình diễn kỹ năng cá nhân thượng thừa.
         </p>
         {params['Watch Now'].value !== '' && (
           <div className="watch-now">
